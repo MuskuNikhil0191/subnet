aff2bd986eb243705dacd27562d9256aff4b850d
Nikhil Reddy <nikhilreddy3111@gmail.com>
2024-01-27 22:33:56 -0500

    sg code

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,10 @@
-output "subnet_id" {
-  description = "subnet id"
-  value       = aws_subnet.public_subnets.id
+output "subnet1_id" {
+  description = "subnet1 id"
+  value       = aws_subnet.public_subnet1.id
+  sensitive   = false
+}
+output "subnet2_id" {
+  description = "subnet2 id"
+  value       = aws_subnet.public_subnet2.id
   sensitive   = false
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "public_subnet_cidrs" {
-  type        = string
+  type        = list(string)
   description = "Public Subnet CIDR values"
-  default     = "10.0.8.0/24"
+  default     = ["10.0.1.0/24", "10.0.10.0/24"]
 }
-variable "public_subnet_name" {
+variable "public_subnet1_name" {
   type    = string
   default = "public-subnet-1"
 }
-variable "public_subnet_azs" {
+variable "public_subnet2_name" {
   type    = string
-  default = "us-east-1a"
+  default = "public-subnet-2"
+}
+variable "public_subnet_azs" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
 }
 variable "vpc__id" {
   type    = string
